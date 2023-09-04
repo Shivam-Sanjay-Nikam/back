cbbd868a6eb1c80820c77ceabd61a9ef789db0c0
prinswiks <shivamsanjaynikam17112002@gmail.com>
2023-09-04 23:03:40 +0530

    hi

---
+++ server.js
@@ -0,0 +1,33 @@
+
+const express=require('express');
+const app=express();
+const cors = require('cors');
+const mongoose=require('mongoose')
+require('dotenv').config();
+const todoroutes=require('./routes/todos')
+const userRoutes = require('./routes/user')
+
+app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log(req.method,req.path);
+    next()
+})
+app.use('/api/todolist',todoroutes);
+app.use('/api/user', userRoutes)
+
+app.use(cors());
+
+
+// connect to db
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
